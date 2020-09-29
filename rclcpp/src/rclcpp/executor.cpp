966741595459f3ba10d9e9a893c8e39e51c52400
Micrified <charles.xrandolph2@gmail.com>
2020-09-29 23:19:40 +0200

    Restored proper licensing for executor source file

--- rclcpp/src/rclcpp/executor.cpp
+++ rclcpp/src/rclcpp/executor.cpp
@@ -1,17 +1,16 @@
-/*
- *******************************************************************************
- *              (C) Copyright 2020 Delft University of Technology              *
- * Created: 29/08/2020                                                         *
- *                                                                             *
- * Programmer(s):                                                              *
- * - Charles Randolph                                                          *
- *                                                                             *
- * Description:                                                                *
- *  Custom RCLCPP executor implementing preemptive callback execution, and cal *
- *  lback priority tables                                                      *
- *                                                                             *
- *******************************************************************************
-*/
+// Copyright 2014 Open Source Robotics Foundation, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 
 // Standard headers
